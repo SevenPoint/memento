2962fa65b85e8f8af1cb8ee354c030eb39695e2b
Mery2013 <gimenez166@hotmail.com>
2013-10-06 18:10:38 -0300

    Commit Test Objetivo

--- memento/test/unit/ar/edu/unlam/ObjetivoSpec.groovy
+++ memento/test/unit/ar/edu/unlam/ObjetivoSpec.groovy
@@ -7,14 +7,24 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Objetivo)
-class ObjetivoSpec extends Specification {
+class ObjetivoSpec{
 
+	def objetivo = new Objetivo(titulo:"Hola Chicos",descripcion:"Esto es un test",categoria:"Test de Test");
+	
     def setup() {
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void testConstraintTitulo() {
+		objetivo.titulo = "Tengo mas de muchos mas caracteres de lo permitido...." //Tengo mas de 50 caracteres 
+		assert !objetivo.validate();
     }
+	
+	void testConstraintDescripcion(){
+		objetivo.descripcion = ""
+		assert !objetivo.validate()
+	}
+	
 }
