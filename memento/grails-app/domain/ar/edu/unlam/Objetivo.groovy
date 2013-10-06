df85fc98f0703319f975e8461eea58efc0f77ac4
Mery2013 <gimenez166@hotmail.com>
2013-10-06 18:08:34 -0300

    Commit Objetivo

--- memento/grails-app/domain/ar/edu/unlam/Objetivo.groovy
+++ memento/grails-app/domain/ar/edu/unlam/Objetivo.groovy
@@ -2,6 +2,21 @@ package ar.edu.unlam
 
 class Objetivo {
 
-    static constraints = {
+	Integer idobjetivo
+	String titulo
+	String descripcion
+	String categoria
+	String idusuario
+	Integer cantPasos
+	Integer cantDias
+	String estado
+    
+	static constraints = {
+		titulo blank:false,size:5..50
+		descripcion blank:false,size:1..256
+		categoria blank:false
+		idusuario blank:false
+		cantPasos blank:false,minvalue:1,maxvalue:40
+		
     }
 }
