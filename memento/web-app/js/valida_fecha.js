acae757615bfd2d480b0144113e3399b11f25524
IvanaL <ivi_ml@hotmail.com>
2013-11-08 23:32:08 -0300

    Detalles Maquetacion

--- memento/web-app/js/valida_fecha.js
+++ memento/web-app/js/valida_fecha.js
@@ -1,12 +1,23 @@
-$(function(){
-	$("#boton").click(function(){
-		var fecha_inicio = $('#input1').val();
-	var fecha_fin = $('#input2').val();
-	
-	if(fecha_inicio > fecha_fin)
-		var e=$("<p>").html("La fecha de incio debe ser menor"); 
-		$("#contenedor").append(e);
-	else
-	return true;
+$(document).ready(function() {
+    $('input[name=fecha_in]').val();
+    $('input[name=fecha_fn]').val();
+    $('input[type=submit]').click(function(e) {
+        var fecha_in = $('input[name=fecha_in]').val(),
+            fecha_fn = $('input[name=fecha_fn]').val(),
+            aFecIn = fecha_in.split('/'),
+            aFecFn = fecha_fn.split('/'),
+            bFecha = false,
+            oDateIn = new Date(aFecIn[2], aFecIn[1], aFecIn[0]),
+            oDateFn = new Date(aFecFn[2], aFecFn[1], aFecFn[0]);
+        if (oDateIn.getTime() > oDateFn.getTime())
+			return false;
+			//$('p.result').text('La fecha de inicio debe ser menor a la de fin');*/
+		else
+		return true;
+
 		
+        /*if (oDateIn.getTime() < oDateFn.getTime())
+            bFecha = true;
+        $('p.result').text(bFecha ? 'SI' :  bFecha = true;);*/
+    });
 });
